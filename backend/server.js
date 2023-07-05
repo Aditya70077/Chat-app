1a0dc340d3e24b10ebde2f67006855377ca7d7ad
Pranay-28 <pranaybambole11@gmail.com>
2023-07-05 18:42:54 +0530

    code

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const app=express();
 
 app.use(express.json());
 
-// app.get('/',(req,res)=>{
-//     res.send("API is running succesfully")
-// });
+app.get('/',(req,res)=>{
+    res.send("API is running succesfully")
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
